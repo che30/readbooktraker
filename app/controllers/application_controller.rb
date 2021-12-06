856095e0f5e8dbc407563e71aceeefbf7f558602
che <che.blancoo58@gmail.com>
2021-12-06 06:37:04 +0100

    Update wrap_parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::API
   include Response
   include ExceptionHandler
-
   # called before every action on controllers
   before_action :authorize_request
   attr_reader :current_user
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Cat.create(name: 'fiction')
+Cat.create(name: 'classics')
