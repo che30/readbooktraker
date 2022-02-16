5aa8f16d3b6451a28e9643273f35d75bc3c08350
che <che.blancoo58@gmail.com>
2022-02-16 12:48:26 +0100

    Update cors endpoints

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,9 +8,26 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'https://readbooktracker.herokuapp.com'
-
-    resource '*',
+    resource '/auth/login',
+      :headers => :any,
+      :methods => [:post]
+    resource '/signup',
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      methods: [:post, :head]
+    resource '/users/:user_id/books',
+      headers: :any,
+      methods: [:get, :head]
+    resource '/users/:user_id/books',
+      headers: :any,
+      methods: [:post, :head]
+    resource '/measurments',
+      headers: :any,
+      methods: [:post, :head]
+    resource '/users/:user_id/measurments',
+      headers: :any,
+      methods: [:get, :head]
+    resource '/cats',
+      headers: :any,
+      methods: [:get, :head]
   end
 end
