9e84143e472df16fd1dce6d4b1e23575c5d70b01
che <che.blancoo58@gmail.com>
2022-02-16 12:02:14 +0100

    Update cors origin spelling

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://readbooktracker.herokuapp.com/'
+    origins 'https://readbooktracker.herokuapp.com'
 
     resource '*',
       headers: :any,
