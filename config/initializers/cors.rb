a11daf664c75bb2e9b0505cebcdd06c1a896b80a
che <che.blancoo58@gmail.com>
2022-02-22 00:35:33 +0100

    Delet errors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -30,6 +30,5 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     resource '/cats',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-      methods: [:get, :head]
   end
 end
