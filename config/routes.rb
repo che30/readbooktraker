d8de97cea93e979f6031cad972fa8a256c0ea63b
che <che.blancoo58@gmail.com>
2022-02-23 06:45:01 +0100

    Update json_web token file

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 
-  require 'api_constraints'
+require 'api_constraints'
 Rails.application.routes.draw do
 namespace :api do
   scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
